690c3d427df468b66f4f5e70e1235e16d587b036
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-10-10 15:46:23 +0200

    initial commit

---
+++ BeaconsDetector/BeaconsDetector/BeaconsDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconsDetectorApp.swift
+//  BeaconsDetector
+//
+//  Created by Amr Hesham on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BeaconsDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
