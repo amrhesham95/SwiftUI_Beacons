6c0eab54ced70124b6960fd9417454c109f076e2
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-10-10 16:28:19 +0200

    update content view

--- BeaconsDetector/BeaconsDetector/ContentView.swift
+++ BeaconsDetector/BeaconsDetector/ContentView.swift
@@ -6,11 +6,25 @@
 //
 
 import SwiftUI
+import Combine
+import CoreLocation
 
 struct ContentView: View {
+    @ObservedObject var detector = BeaconDetector()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+ 
+        switch detector.lastDistance {
+        case CLProximity.immediate:
+            BeaconStateView(beaconState: "immediate")
+        case .unknown:
+            BeaconStateView(beaconState: "unknown")
+        case .near:
+            BeaconStateView(beaconState: "near")
+        case .far:
+            BeaconStateView(beaconState: "far")
+        @unknown default:
+            BeaconStateView(beaconState: "unknown")
+        }
     }
 }
 
